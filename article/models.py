7b3ab78d43ae2e7d401051ff562c986d0a787dd7
dimtirhusev <seaofdreams@mail.ru>
2017-11-03 00:51:40 +0200

    error does not appear if imahe was cleared

--- article/models.py
+++ article/models.py
@@ -4,7 +4,7 @@ class Article(models.Model):
 
     title = models.CharField(max_length=200)
     body = models.TextField()
-    image = models.FileField(upload_to='documents/%Y/%m/%d/', null=True, blank=True)
+    image = models.FileField( null=True, blank=True)
 
     def __str__(self):
         return '%s %s' %(self.title, self.body)
