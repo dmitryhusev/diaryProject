67c69d575d7d8d01d88f8111e6a45ad3e1e8aa91
dimtirhusev <seaofdreams@mail.ru>
2017-11-14 01:50:46 +0200

    added tracker func: add/edit issues

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-from .views import main,issue
+from .views import main,issues, edit_issue, add_issue
 
 urlpatterns = [
     url(r'^tracker/$', main, name='main'),
-    url(r'^issue/(?P<article_id>\d+)/$', issue, name='issue'),
+    url(r'^issues/$', issues, name='issues'),
+    url(r'^edit/issue/(?P<issue_id>\d+)/$', edit_issue, name='edit_issue'),
+    url(r'^add/issue/$', add_issue, name='add_issue'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.contrib.auth.models import User
 from .forms import AssignForm
+from .models import Tracker
 
 def main(request):
 
@@ -9,6 +12,36 @@ def main(request):
     context = {'users': user_list, 'form': form}
     return render(request, 'tracker.html', context)
 
-def issue(request):
+def issues(request):
 
-    pass
+    issues = Tracker.objects.all()
+    context = {'issues': issues}
+    return render(request, 'issues.html', context)
+
+
+def add_issue(request):
+
+    user_list = User.objects.all()
+    if request.method != 'POST':
+        form = AssignForm()
+    else:
+        form = AssignForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('tracker:issues'))
+    context = {'form': form, 'users': user_list, }
+    return render(request, 'add_issue.html', context)
+
+def edit_issue(request, issue_id):
+
+    user_list = User.objects.all()
+    issue = Tracker.objects.get(id=issue_id)
+    if request.method != 'POST':
+        form = AssignForm(instance = issue)
+    else:
+        form = AssignForm(instance = issue, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('tracker:edit_issue', args=[issue_id]))
+    context = {'form': form, 'users': user_list, 'issue_id': issue_id}
+    return render(request, 'edit_issue.html', context)
