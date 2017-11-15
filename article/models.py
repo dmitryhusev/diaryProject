fdc28abac555948ba8b477f8edd018b7ddaa5f96
dimtri <seaofdreams@mail.ru>
2017-11-15 15:42:29 +0200

    migrations config

--- article/models.py
+++ article/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 class Article(models.Model):
+
 
     title = models.CharField(max_length=200)
     body = models.TextField()
-    image = models.FileField( null=True, blank=True)
-    date_added = models.DateTimeField(auto_now_add=True, null=True)
+    image = models.FileField(null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
--- tracker/models.py
+++ tracker/models.py
@@ -6,7 +6,7 @@ class Tracker(models.Model):
 
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=1000)
-    assignee = models.CharField(max_length=30)
+    assignee = models.CharField(max_length=35)
     date_added = models.DateTimeField(auto_now_add=True, null=True)
 
 
