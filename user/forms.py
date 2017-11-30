d7f19dfaaffb9d99061d8f3ed47d796dec26afe1
dimtirhusev <seaofdreams@mail.ru>
2017-12-01 01:49:49 +0200

    footer added, validation on edit profile added

--- user/forms.py
+++ user/forms.py
@@ -5,6 +5,11 @@ from django import forms
 class ProfileForm(forms.ModelForm):
 
 
+    first_name = forms.CharField(max_length=15, required=True)
+    last_name = forms.CharField(max_length=15, required=True)
+    email = forms.EmailField(required=True)
+
+
     class Meta:
         model = User
         fields = ['first_name', 'last_name','email']
