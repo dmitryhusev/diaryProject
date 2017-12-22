dcda226bda31cabd6921b341526c3cda89a26de2
dmitryhusev <seaofdreams@mail.ru>
2017-12-22 14:32:12 +0300

    flake8 on article *.py

--- article/forms.py
+++ article/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Article, ArticleCategory
 
+
 class ArticleForm(forms.ModelForm):
 
     category = forms.ModelChoiceField(
@@ -10,6 +11,7 @@ class ArticleForm(forms.ModelForm):
         help_text='Choose article category',
         )
     image = forms.FileField(required=False)
+
     class Meta:
         model = Article
         fields = ['title', 'body', 'category', 'image']
@@ -17,18 +19,21 @@ class ArticleForm(forms.ModelForm):
             'title': 'Article name',
             'body': 'Enter article body here'}
 
-'''def __init__(self, *args, **kwargs):
-        super(ArticleForm, self).__init__(*args, **kwargs)
-        self.fields['category'].empty_label = 'Select'
-
-'''
 
 class ArticleCategoryForm(forms.ModelForm):
 
     title = forms.CharField(
         label='Category name',
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Category name'}))
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Category name'}))
 
     class Meta:
         model = ArticleCategory
         fields = ['title']
+
+
+'''def __init__(self, *args, **kwargs):
+        super(ArticleForm, self).__init__(*args, **kwargs)
+        self.fields['category'].empty_label = 'Select'
+'''
--- article/urls.py
+++ article/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from .views import article_list, add_article, edit_article,\
-delete_article, view_article, add_category, delete_category, edit_category, category
+    delete_article, view_article, add_category, delete_category,\
+    edit_category, category
 
 urlpatterns = [
     url(r'^list/$', article_list, name='list'),
@@ -12,6 +13,4 @@ urlpatterns = [
     url(r'^category/(?P<category_id>\d+)/$', category, name='category'),
     url(r'^edit/category/(?P<category_id>\d+)/$', edit_category, name='edit_category'),
     url(r'^delete/category/(?P<category_id>\d+)/$', delete_category, name='delete_category'),
-    
-
 ]
