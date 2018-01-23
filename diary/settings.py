7337b1eb1234a08b792b17be7b8288dd843f0fdd
dimtirhusev <seaofdreams@mail.ru>
2018-01-23 23:44:19 +0200

    ALLOWED_HOSTS changed

--- diary/settings.py
+++ diary/settings.py
@@ -27,12 +27,7 @@ SECRET_KEY = '@er76-jkys66j#)b3_n961^ha3&cl4ky1#@p1jb9rcajufye4i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'dimtri.pythonanywhere.com',
-    '127.0.0.1',
-    'lwtrack.pythonanywhere.com',
-    'pepathepig.herokuapp.com',
-    ]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
